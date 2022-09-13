ba14620a966ada3c70d5fb3c85b6177d9fcea465
Vorshik <58830879+Vorshik@users.noreply.github.com>
2022-09-13 15:51:45 +0400

    Fix template of the report. Add new command 'leave'. (#3)
    * Telegram bot that retrieve reports from H1
    
    * Added information about bounties into a report and changed the photo upload. Added new command /leave.

--- bot/bot.py
+++ bot/bot.py
@@ -34,11 +34,12 @@ def tel_send_message(chat_id,payload):
     return r
 
 def tel_send_photo(chat_id,photoURL,caption):
-    URL = f'https://api.telegram.org/bot{TOKEN}/sendPhoto'
+
+    URL = f'https://api.telegram.org/bot{TOKEN}/sendDocument'
 
     payload = {
         'chat_id': chat_id,
-        'photo': photoURL,
+        'document': photoURL,
         'caption': caption,
         'parse_mode':'HTML'
     }
@@ -55,7 +56,8 @@ def index():
 
         try:
             if(command == '/start'):
-                GreetingMsg = "<b>Hello! Glad to see you here!\n\rI'll be sending you reports of hacktivity from H1\n\rUse /help to see all capabilities.</b>"
+
+                GreetingMsg = f"<b>Hello {msg['message']['from']['username']}! Glad to see you here!\n\rI'll be sending you reports of hacktivity from H1\n\rUse /help to see all capabilities.</b>"
                 tel_send_message(chat_id, GreetingMsg)
             
             elif(command == '/update'):
@@ -83,7 +85,7 @@ def index():
                         continue
 
                     elif DupReport:
-                        reportTemplate = f"<b>\n\r\n\r{report['node']['report']['title']}\n\r</b><b>\n\r&#9889; {report['node']['report']['url']}\n\r</b><b>\n\r&#10071; Severity: {report['node']['severity_rating']}\n\r</b><b>&#10062; Reported to: <a href=\"{report['node']['team']['url']}\">{report['node']['team']['name']}</a>\n\r</b><b>&#9937; Reported by: {report['node']['reporter']['username']}\n\r</b><b>&#8987; Disclosed at: {datetime.strptime(report['node']['latest_disclosable_activity_at'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%B %d %Y')}</b>"
+                        reportTemplate = f"<b>\n\r\n\r{report['node']['report']['title']}\n\r</b><b>\n\r&#9889; {report['node']['report']['url']}\n\r</b><b>\n\r&#10071; Severity: {report['node']['severity_rating']}\n\r</b><b>&#128181; Bounty: {report['node']['total_awarded_amount']} {report['node']['currency']}\n\r</b><b>&#10062; Reported to: <a href=\"{report['node']['team']['url']}\">{report['node']['team']['name']}</a>\n\r</b><b>&#9937; Reported by: {report['node']['reporter']['username']}\n\r</b><b>&#8987; Disclosed at: {datetime.strptime(report['node']['latest_disclosable_activity_at'], '%Y-%m-%dT%H:%M:%S.%fZ').strftime('%B %d %Y')}</b>"
                         tel_send_photo(chat_id, report['node']['team']['medium_profile_picture'], reportTemplate)
                         dbrecord.HackerOne_report_id = report['node']['id']
                         isUpdated = True
@@ -97,6 +99,17 @@ def index():
                 else:
                     Message = "<b>You're already up to date with H1 database!</b>"
                     tel_send_message(chat_id, Message)
+
+            elif(command == '/leave'):
+
+                dbrecord = DuplicateReport.query.filter_by(chat_id=chat_id).first()
+                Message = f"<b>It was pleasure to work with you {msg['message']['from']['username']}! &#128546;\n\r\n\rAs you decided to leave the bot make the following actions:\n\r\n\r1. Open the bot menu and tap on 'Stop Bot' or 'Delete Chat' button.\n\r2. On your wish you can delete Chat with me by tapping on 'Clear messages' in the bot's menu.</b>"
+
+                if dbrecord:
+                    db.session.delete(dbrecord)
+                    db.session.commit()
+
+                tel_send_message(chat_id, Message)
 
             elif(command == '/help'):
                 tel_send_message(chat_id, "<b>Use these commands to interact with the bot.\n\r/start - Use this command to start your bot.\n\r/update - Use this command to initiate an update process from H1 (It make take several seconds to run this command).\n\r/help - Use this command to display bot's possibility.</b>")
